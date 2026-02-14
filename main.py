d9109411c7098126ad76c58567b6d976f25d1688
Arshad Ali <arshadali@Arshads-MacBook-Air.local>
2026-02-14 19:38:10 +0530

    fst

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastmcp import FastMCP
+
+mcp=FastMCP("simple calculator")
+
+
+@mcp.tool
+def add(a:int,b:int):
+    """this tool is used to do the addition here"""
+    return a+b
+
+
+if __name__=="__main__":
+    mcp.run(transport="http",host="0.0.0.0",port=8080)
